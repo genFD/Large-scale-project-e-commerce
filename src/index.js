89e65afa7032b8c1cc73232ab637d7b37b0b8da1
hermkan <hermann.kanga.dev@gmail.com>
2021-10-12 16:13:58 -0400

    used redux-persist library to setup local storage for cart

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'; //component that wraps around the top level add so that we can access the store from anywhere in our app.
-import store from './redux/store/store';
+import { persistor, store } from './redux/store/store';
+
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import rootReducer from '../root-reducer';
+import { persistStore } from 'redux-persist';
 
 ////////////////////////////////////////
 // const middleWares = [logger];
-const store = createStore(rootReducer, applyMiddleware(logger));
+export const store = createStore(rootReducer, applyMiddleware(logger));
 
-export default store;
+export const persistor = persistStore(store);
