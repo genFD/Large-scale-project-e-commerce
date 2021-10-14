b59acf125276cc337cd2f4faafd8cf97df56258d
hermkan <hermann.kanga.dev@gmail.com>
2021-10-13 20:27:56 -0400

    Normalize data + updated favicon

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-// import { logger } from 'redux-logger';
-import { createLogger } from 'redux-logger';
+import { logger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import rootReducer from '../root-reducer';
 import { persistStore } from 'redux-persist';
 
 ////////////////////////////////////////
-// const middleWares = [logger];
-const logger = createLogger();
+const middleWares = [];
+if (process.env.NODE_ENV === 'deployment') {
+  middleWares.push(logger);
+}
+// const logger = createLogger();
 export const store = createStore(rootReducer, applyMiddleware(logger));
 
 export const persistor = persistStore(store);
