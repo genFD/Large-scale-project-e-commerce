06e3b266ef92ccaf54eab454254e5030c17c569a
hermkan <hermann.kanga.dev@gmail.com>
2021-10-18 19:44:13 -0400

    converted header + custom button into styled components

--- src/Components/custom-button/custom-button.component.jsx
+++ src/Components/custom-button/custom-button.component.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import './custom-button.styles.scss'
-
-const CustomButton = ({inverted,children, isGoogleSignIn,...otherProps})=>{
+import { CustomButtonContainer } from './custom-button.styles'
+const CustomButton = ({children, ...props})=>{
  // if the props isGoogleSignIn is true, we'll have a button with google-sign-in class as well as custom button, otherwise it's just going to be custom-button
  return(
- <button className=
- {`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
- {...otherProps}
+ <CustomButtonContainer {...props}
  >
   {children}
- </button>
+ </CustomButtonContainer>
  
  )
 }
